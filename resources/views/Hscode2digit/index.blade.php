6db0061c28ba2b1cb0610336b0ad3cdae71fdba7
fuji-Yuto <fujie.yuto.104@s.kyushu-u.ac.jp>
2023-12-20 00:18:25 +0900

    edit CSS

--- resources/views/Hscode2digit/index.blade.php
+++ resources/views/Hscode2digit/index.blade.php
@@ -4,11 +4,23 @@
             第{{ $number }}類
         </h2>
     </x-slot>
-
-    @foreach($Item as $item)
-        <p>{{$item -> HScode_4}}</p>
-        <p>{{$item -> description}}</p>
-    @endforeach
+    <div class = "mx-auto">
+        <table class = "table table-striped" >
+            <thead>    
+                <tr>
+                    <th>HScode上4桁</th><th>品目</th>
+                </tr>
+            </thead>
+            @foreach($Item as $item)
+            <tbody>
+                <tr>
+                    <td>{{$item -> HScode_4}}</td>
+                    <td>{{$item -> description}}</td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+    </div>
 
 
 </x-app-layout>
