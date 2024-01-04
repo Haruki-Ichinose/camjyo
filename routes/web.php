20fd2b84b7c978969195d13479ad52df865832cf
Haruki-Ichinose <haruichi0712@icloud.com>
2024-01-04 22:28:24 +0900

    storage

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Hscode_2digitController;
 use App\Http\Controllers\Hscode_6digitController;
+use App\Http\Controllers\FileUploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,5 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/upload', [FileUploadController::class, 'uploadForm'])->name('upload.form');
+Route::post('/upload', [FileUploadController::class, 'upload'])->name('upload');
 
 require __DIR__.'/auth.php';
