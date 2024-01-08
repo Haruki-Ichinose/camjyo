c37721e10d133688149dfb205417f5119b0d3d76
千尋 <143112530+mokomokonomoko@users.noreply.github.com>
2024-01-09 01:14:56 +0900

    dashboardの整理

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,37 @@
+<!-- resources/views/your/view.blade.php -->
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+         {{ __("HOME") }}
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-teal-600 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
-            
-            <!--ボタンタグの部分-->
-            @for ($i = 1; $i <= 10; $i++)
-                <form method="POST" action="{{ route('2digit.index' , ['number' => $i] ) }}" >
-                    @csrf
-                    <button class="btn btn-outline-primary  btn-lg" type="submit" >第{{$i}}類</button>
-                </form>
-            @endfor
+
+            <!-- ボタンの部分 -->
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @for ($i = 1; $i <= 10; $i++)
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-header fs-3">第{{$i}}類</h5>
+                                <p class="card-text">
+                                    {{ (new \App\Models\HScode_4digits())->getText($i) }}
+                                </p>
+                                <form method="POST" action="{{ route('2digit.index', ['number' => $i]) }}">
+                                    @csrf
+                                    <button class="btn btn-outline-primary btn-lg" type="submit">次へ</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+            </div>
         </div>
     </div>
-    
 </x-app-layout>
